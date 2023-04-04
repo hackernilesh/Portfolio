bbf73a03946c29be147d9d1108a673188af91898
nileshmalav <nileshmalav2020@gmail.com>
2023-04-04 20:02:55 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -28,30 +28,15 @@
   }
 
 
-
-
-  /**
-   * Hero type effect
-   */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
-
   /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
     AOS.init({
+      startEvent: 'DOMContentLoaded',
+      ease:"linear",
     })
   });
 
 })()
+
--- js/scroll.js
+++ js/scroll.js
@@ -9,13 +9,7 @@ var h = document.documentElement,
 	 scroll;
 
 document.addEventListener('scroll', function() {
-	// console.log(h, b);
-	// let's calculate the scroll position
-	// ( body.scrollTop / ( body.scrollHeight - document.height ) ) * 100
-	// multiplying by 100 here gives us a percent value
 	scroll = ( h[st] || b[st] ) / ( ( h[sh] || b[sh] ) - h.clientHeight ) * 100;
-	
-	// set our variable for CSS to use
 	htop.style.setProperty('--scroll', scroll + '%');
 	hbot.style.setProperty('--scroll', scroll + '%');
 	vleft.style.setProperty('--scroll', scroll + '%');
