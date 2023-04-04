e8b95fa1db3c1bc1166692b1742e3f0e93f0e96b
nileshmalav <nileshmalav2020@gmail.com>
2023-04-04 15:33:51 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -52,9 +52,7 @@
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
+      // easing: 'ease-in-out',
     })
   });
 
